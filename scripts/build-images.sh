2585551decadd76497e7c91e27c090f770c9ac86
Artyom <38377534+ksydex@users.noreply.github.com>
2023-02-18 14:20:26 +0300

    fixed build images

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -1,9 +1,8 @@
 version=1.0
 
 # shellcheck disable=SC2164
-cd ./src
 
-docker buildx build --push -t ksydex/gateway-service:$version --platform=linux/amd64 -f ./Gateway/Dockerfile .
-docker buildx build --push -t ksydex/loyalty-service:$version --platform=linux/amd64 -f ./LoyaltyService/Dockerfile .
-docker buildx build --push -t ksydex/payment-service:$version --platform=linux/amd64 -f ./PaymentService/Dockerfile .
-docker buildx build --push -t ksydex/reservation-service:$version  --platform=linux/amd64 -f ./ReservationService/Dockerfile .
+docker buildx build --push -t ksydex/gateway-service:$version --platform=linux/amd64 -f ./src/Gateway/Dockerfile .
+docker buildx build --push -t ksydex/loyalty-service:$version --platform=linux/amd64 -f ./src/LoyaltyService/Dockerfile .
+docker buildx build --push -t ksydex/payment-service:$version --platform=linux/amd64 -f ./src/PaymentService/Dockerfile .
+docker buildx build --push -t ksydex/reservation-service:$version  --platform=linux/amd64 -f ./src/ReservationService/Dockerfile .
